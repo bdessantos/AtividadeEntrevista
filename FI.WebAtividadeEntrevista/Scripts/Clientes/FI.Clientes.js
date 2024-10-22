48d46fd285cb05670cabc25fd13611fbb8d9d5bb
Bruno Santos <bdessantos@hotmail.com>
2024-10-21 22:16:17 -0300

    Inicia criação do modal beneficiario

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -61,3 +61,34 @@ function ModalDialog(titulo, texto) {
     $('body').append(texto);
     $('#' + random).modal('show');
 }
+    
+function AbrirModalBenediciario() {
+
+    //$('body').append(texto);
+    $('#modalBeneficiario').modal('show');
+
+}
+
+function ModalBeneficiarios() {
+
+    var random = Math.random().toString().replace('.', '');
+
+    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
+        '        <div class="modal-dialog">                                                                                 ' +
+        '            <div class="modal-content">                                                                            ' +
+        '                <div class="modal-header">                                                                         ' +
+        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
+        '                    <h4 class="modal-title">Beneficiários</h4>                                                    ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-body">                                                                           ' +
+        '                    <p>' + texto + '</p>                                                                           ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-footer">                                                                         ' +
+        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
+        '                                                                                                                   ' +
+        '                </div>                                                                                             ' +
+        '            </div><!-- /.modal-content -->                                                                         ' +
+        '  </div><!-- /.modal-dialog -->                                                                                    ' +
+        '</div> <!-- /.modal -->                                                                                        ';
+
+}
